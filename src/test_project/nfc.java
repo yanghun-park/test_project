5b943ff462fb1a931fa575faf174a5da97cc3cc5
yanghun-park <didgnsdl6784@naver.com>
2021-04-15 12:09:00 +0900

    [ADD] 안내메시지 추가

--- src/test_project/nfc.java
+++ src/test_project/nfc.java
@@ -3,11 +3,13 @@ package test_project;
 public class nfc {
 	public boolean nfc_connect() {
 		System.out.println("띄익!");
+		System.out.println("연결 되었습니다. ");
 		return true;
 	}
 	
 	public boolean nfc_connectloss() {
 		System.out.println("뚝");
+		System.out.println("연결이 끊겼습니다. ");
 		return true;
 	}
 }
