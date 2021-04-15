c9ff6027cec19a875dfe1026f54b7ecde6111e39
yanghun-park <didgnsdl6784@naver.com>
2021-04-15 17:51:52 +0900

    블루투스 연결완료 메시지 추가

--- src/test_project/bluetooth.java
+++ src/test_project/bluetooth.java
@@ -3,6 +3,7 @@ package test_project;
 public class bluetooth {
 	public boolean bluetooth_connect() {
 		System.out.println("블루투스에 연결합니다. ");
+		System.out.println("블루투스에 연결되었습니다. ");
 		return true;
 	}
 }
