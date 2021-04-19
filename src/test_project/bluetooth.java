9bb0b6fc8bfb7fe2b44f6681fcf9081b513d1f38
yanghun-park <didgnsdl6784@naver.com>
2021-04-19 17:06:35 +0900

    블루투스git add * 문구 추가

--- src/test_project/bluetooth.java
+++ src/test_project/bluetooth.java
@@ -4,6 +4,7 @@ public class bluetooth {
 	public boolean bluetooth_connect() {
 		System.out.println("블루투스에 연결합니다. ");
 		System.out.println("블루투스에 연결되었습니다. ");
+		System.out.println("블루투스!!");
 		return true;
 	}
 }
